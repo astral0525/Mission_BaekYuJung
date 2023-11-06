15f0078ab9acea1b837c3992d06449049a4307a9
astral0525 <astral0525@gmail.com>
2023-11-07 00:55:36 +0900

    step 4

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,6 +4,7 @@ package com.ll;
 import java.util.Scanner;
 
 class App {
+    static int lastId=0;
     void run() {
         while(true) {
             System.out.println("== 명언 앱==");
@@ -11,7 +12,7 @@ class App {
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
-            int n=0;
+
 
 
             if(cmd.equals("종료")){
@@ -24,9 +25,9 @@ class App {
 
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
-                n++;
+                lastId++;
 
-                System.out.printf("%d번 명언이 등록되었습니다.\n", n);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
 
                 //System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
 
