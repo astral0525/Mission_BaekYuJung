8fb57aff548c4fb2c1e750ff6b04b9212217d741
astral0525 <astral0525@gmail.com>
2023-11-06 10:26:38 +0900

    App Class

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,12 @@
 package com.ll;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        new App().run();}
+}
+
+class App{
+    void run(){
+        System.out.println("프로그램 실행");
     }
 }
