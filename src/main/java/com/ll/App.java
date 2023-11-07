77bc35696de9887d9a1d17f6dd23f61b1ad78e1f
astral0525 <astral0525@gmail.com>
2023-11-07 17:24:55 +0900

    step5

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,14 +1,19 @@
 package com.ll;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
-    int lastId = 0;
+
 
     void run() {
+        int lastId = 0;
+        List<Quotation> quotations = new ArrayList<>();
+        System.out.println("== 명언 앱==");
+
         while (true) {
-            System.out.println("== 명언 앱==");
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
@@ -27,10 +32,18 @@ class App {
                 lastId++;
 
                 Quotation quotation = new Quotation(lastId, content,authorName);
+                quotations.add(quotation);
 
 
-                //System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
 
+            } else if (cmd.equals("목록")) {
+                System.out.printf("번호 / 작가 / 명언\n");
+                System.out.printf("---------\n");
+                for(int i= quotations.size()-1; i>=0; i--){
+                    System.out.printf("%d / %s / %s \n", quotations.get(i).id, quotations.get(i).authorName, quotations.get(i).content);
+                }
+                
             }
 
 
