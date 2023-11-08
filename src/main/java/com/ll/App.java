94ee57fa64d3481368f3729142ff7d6df7b12abc
astral0525 <astral0525@gmail.com>
2023-11-08 13:40:01 +0900

    step 6

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -19,22 +19,23 @@ class App {
 
             if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 actionWrite();
-
             } else if (cmd.equals("목록")) {
                 actionList();
-
-            }else if(cmd.equals("삭제")){
-
-           }
+            } else if (cmd.startsWith("삭제?")) {
+                actionRemove(cmd);
+            } else if (cmd.startsWith("수정?")) {
+                actionModify(cmd);
+            } else {
+                System.out.println("등록되지 않은 명령입니다. 다시 입력해주십시오");
+            }
 
 
         }
     }
 
-    void actionWrite(){
+    void actionWrite() {
         //명언, 작가 입력
         System.out.print("명언 : ");
         String content = scanner.nextLine();
@@ -43,23 +44,74 @@ class App {
         lastId++;
 
         //명언객체 생성 및 list추가
-        Quotation quotation = new Quotation(lastId, content,authorName);
+        Quotation quotation = new Quotation(lastId, content, authorName);
         quotations.add(quotation);
         System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
 
     }
 
-    void actionList(){
+    void actionList() {
 
         System.out.printf("번호 / 작가 / 명언\n");
         System.out.printf("---------\n");
 
-        if(quotations.isEmpty())
+        if (quotations.isEmpty())
             System.out.println("등록된 명언이 없습니다.");
 
-        for(int i= quotations.size()-1; i>=0; i--){
+        for (int i = quotations.size() - 1; i >= 0; i--) {
             System.out.printf("%d / %s / %s \n", quotations.get(i).id, quotations.get(i).authorName, quotations.get(i).content);
         }
+    }
+
+    void actionRemove(String cmd) {
+        int id = getParamAsInt(cmd, "id", 0);
+
+        if (id == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        } else if ((id <= lastId)&&(id>0)) {
+            System.out.printf("%d번 명언을 삭제합니다.\n", id);
+        } else {
+            System.out.printf("id가 없습니다.\n");
+        }
+
+    }
+
+    void actionModify(String cmd) {
+        int id = getParamAsInt(cmd, "id", 0);
+
+        if (id == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        } else if ((id <= lastId)&&(id>0)) {
+            System.out.printf("%d번 명언을 수정합니다.\n", id);
+        } else {
+            System.out.printf("id가 없습니다.\n");
+        }
+
+    }
+
+    int getParamAsInt(String cmd, String paramName, int defaultValue) {
+        String[] cmdBits = cmd.split("\\?", 2); //'?'기준으로 2번만 분할
+        String queryString = cmdBits[1]; //"?d이후내용"
+
+        String[] queryStringBits = queryString.split("&");
+        for (int i = 0; i < queryStringBits.length; i++) {
+            String queryParamStr = queryStringBits[i];
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+
+            try {
+                String _paramName = queryParamStrBits[0];
+                String paramValue = queryParamStrBits[1];
+                if (_paramName.equals(paramName)) {
+                    return Integer.parseInt(paramValue);
+                }
+            } catch (Exception e) {
+                return defaultValue;
+            }
+        }
+
+        return defaultValue;
     }
 
 
